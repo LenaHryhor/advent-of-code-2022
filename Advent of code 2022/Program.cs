3c7d81387595e4499596e5efccfd3ee401728587
Olena Grigor <olena.grigor@atola.com>
2022-12-06 09:58:41 +0200

    day3

--- Advent of code 2022/Program.cs
+++ Advent of code 2022/Program.cs
@@ -1,14 +1,13 @@
-﻿using Advent_of_code_2022.day_2;
-using Advent_of_code_2022.day1;
+﻿using Advent_of_code_2022.day3;
 using System;
 
-namespace MyApp // Note: actual namespace depends on the project name.
+namespace Advent_of_code_2022
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Day2.Run();
+            Day3.Run();
         }
     }
 }
---
+++ Advent of code 2022/day3/Day3.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace Advent_of_code_2022.day3
+{
+    internal static class Day3
+    {
+        // a through z : 1 through 26.
+        // A through Z : 27 through 52.
+
+        private static int GetPriorityChar(char item) => Char.IsLower(item) ? item - 'a' + 1 : item - 'A' + 1 + 27;
+
+        public static void Run()
+        {
+            try
+            {
+                StreamReader sr = new StreamReader("C:\\dev\\advent-of-code-2022\\Advent of code 2022\\day3\\day3.txt");
+                String line = "";
+
+                int sum = 0;
+                int checker = 0;
+                List<string> list = new List<string>();
+                while (line != null)
+                {
+                    line = sr.ReadLine();
+                    list.Add(line);
+                    
+                    if (checker == 3)
+                    {
+                        var firstDiff = list[0].Intersect(list[1]);
+                        var res = firstDiff.Intersect(list[2]).Distinct().First();
+
+                        sum += GetPriorityChar(res);
+
+                        checker = 0;
+                        list.Clear();
+                    }
+
+                    checker++;
+                }
+                sr.Close();
+
+
+                var res1 = list[0].Intersect(list[1]).ToList().Intersect(list[2]).Distinct().First();
+
+                sum += GetPriorityChar(res1);
+
+                Console.WriteLine(sum);
+
+                //part 1
+                Console.WriteLine($"Total part1: {sum}");
+
+                //part 2
+                //Console.WriteLine($"Total part2: {sumPart2}");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception: " + e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Executing finally block.");
+            }
+
+        }
+    }
+}
