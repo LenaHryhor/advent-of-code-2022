7c2041a60e6fa2e2792679f90240e190378691c9
Olena Grigor <olena.grigor@atola.com>
2022-12-06 09:46:32 +0200

    day 1, day2

---
+++ Advent of code 2022/day1/Day1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advent_of_code_2022.day1
+{
+    internal static class Day1
+    {
+        private static int FindMaxValue(int exeptFirst = -1, int exeptSecond = -1)
+        {
+            StreamReader sr = new StreamReader("C:\\dev\\advent-of-code-2022\\Advent of code 2022\\day1\\day1.txt");
+
+            String line = sr.ReadLine();
+            var maxValue = 0;
+            var sum = 0;
+
+            while (line != null)
+            {
+                if (int.TryParse(line, out int number))
+                {
+                    sum += number;
+                }
+                else
+                {
+                    if (sum > maxValue && sum != exeptFirst && sum != exeptSecond)
+                        maxValue = sum;
+                    sum = 0;
+                }
+
+                line = sr.ReadLine();
+            }
+
+            sr.Close();
+            return maxValue;
+
+        }
+
+        //part 1:  Find the Elf carrying the most Calories. How many total Calories is that Elf carrying?
+        //part 2:  Find the top three Elves carrying the most Calories. How many Calories are those Elves carrying in total?
+        public static void Run()
+        {
+            try
+            {
+                //part 1
+                Console.WriteLine($"Total calories: {FindMaxValue()}");
+
+                //part 2
+                var firstCal = FindMaxValue();
+                var secondCal = FindMaxValue(firstCal);
+                var thirdCal = FindMaxValue(firstCal, secondCal);
+                Console.WriteLine($"Total top three: {firstCal + secondCal + thirdCal}");
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception: " + e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Executing finally block.");
+            }
+
+        }
+    }
+}
---
+++ Advent of code 2022/day2/Day2.cs
@@ -0,0 +1,118 @@
+﻿using System;
+
+namespace Advent_of_code_2022.day_2
+{
+    internal static class Day2
+    {
+        // камінь перемагає ножиці
+        // ножиці перемагають папір
+        // папір перемагає камінь
+
+        //A - камінь, B - папір, C - ножиці. - opponent
+        //X - камінь, Y - папір, Z - ножиці. - I
+
+        //1 - камінь, 2 - папір, and 3 - ножиці
+        // outcome of the round (0 if you lost, 3 - was a draw, and 6 - you won
+
+
+        //part 2
+        //X - lose, Y - a draw, and Z - win
+
+        private static int GetWinScores(string opponent, string myAnsw)
+        {
+            if (opponent == "A" && myAnsw == "X" ||
+                opponent == "B" && myAnsw == "Y" ||
+                opponent == "C" && myAnsw == "Z") // was a draw
+                return 3;
+
+            if (opponent == "A" && myAnsw == "Z" ||
+                opponent == "B" && myAnsw == "X" ||
+                opponent == "C" && myAnsw == "Y") // lost
+                return 0;
+
+            return 6; // you won
+        }
+        private static int GetShapeScores(string shape)
+        {
+            if (shape == "X")
+                return 1;
+            if (shape == "Y")
+                return 2;
+            return 3; // "Z"
+        }
+
+        //part 2
+        private static string GetMyAnswer(string roundResult, string opponent)
+        {
+            if (roundResult == "X") //lose
+            {
+                if (opponent == "A")
+                    return "Z";
+                if (opponent == "B")
+                    return "X";
+                if (opponent == "C")
+                    return "Y";
+            }
+            else if (roundResult == "Y") //draw
+            {
+                if (opponent == "A")
+                    return "X";
+                if (opponent == "B")
+                    return "Y";
+                if (opponent == "C")
+                    return "Z";
+            }
+            else if (roundResult == "Z") //win
+            {
+                if (opponent == "A")
+                    return "Y";
+                if (opponent == "B")
+                    return "Z";
+                if (opponent == "C")
+                    return "X";
+            }
+            return "";
+        }
+
+        public static void Run()
+        {
+            try
+            {
+                StreamReader sr = new StreamReader("C:\\dev\\advent-of-code-2022\\Advent of code 2022\\day2\\day2.txt");
+
+                String line = sr.ReadLine();
+                var sumPart1 = 0;
+                var sumPart2 = 0;
+
+                while (line != null)
+                {
+                    var list = line.Split(' ');
+                    sumPart1 = sumPart1 + GetWinScores(list[0], list[1]) + GetShapeScores(list[1]);
+                    
+                    var myAnswer = GetMyAnswer(list[1], list[0]);
+                    sumPart2 = sumPart2 + GetWinScores(list[0], myAnswer) + GetShapeScores(myAnswer);
+
+                    line = sr.ReadLine();
+                }
+
+                sr.Close();
+
+
+                //part 1
+                Console.WriteLine($"Total part1: {sumPart1}");
+
+                //part 2
+                Console.WriteLine($"Total part2: {sumPart2}");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception: " + e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Executing finally block.");
+            }
+
+        }
+    }
+}
